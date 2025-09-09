6158df3b600269e3e2a6a82bedee00f35d117014
LuisRoca11 <lroca1102@gmail.com>
2025-09-09 15:06:12 -0300

    fecha de navidad

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Realizar un programa que pida cargar una fecha cualquiera, luego verificar si dicha fecha corresponde a Navidad.
+
+using System;
+
+namespace fecha_navidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una fecha (dd/mm)");
+            string fecha = Console.ReadLine();
+            string navidad = "25/12";
+
+            if (fecha == navidad)
+            {
+                Console.WriteLine("Esta fecha corresponde a navidad");
+            }
+            else
+            {
+                Console.WriteLine("Esta fecha no es navidad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
